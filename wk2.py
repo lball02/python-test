a50cd94bc396d7a18c03828627ff9747c8953f2f
lball02 <lball02@gmail.com>
2020-10-13 16:07:10 +0000

    week 2 practices

---
+++ wk2.py
@@ -0,0 +1,35 @@
+# dict = [{
+#     "name":"John Doe", 
+#     "age": 21, 
+#     "net worth": 5213.4
+#     },
+#     {
+#     "name":"Pamela", 
+#     "age": 35, 
+#     "net worth": 6000
+#     }]
+# print(type(dict))
+# print(dict)
+# print(dict[1]["net worth"])
+name = True
+print(name)
+print(type(name))
+list = [1, 2, 3, 4]
+list.append(5)
+print(type(list))
+print(list[4])
+
+#name = "John"
+#print(type(name))
+dict = [{
+        "name": "John Doe",
+        "age": 21, 
+        "net worth": 5213.4
+        },
+        {
+        "name": "Pamela Kavanagh",
+        "age": 21, 
+        "net worth": 5213.4
+        }]
+print(dict)
+print(dict[1]["name"])
